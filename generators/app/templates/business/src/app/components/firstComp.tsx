2107411eec35e68d2a8967f5aa889cf82c7f2d1e
陈焱 <alberteinstein007@126.com>
2020-11-10 10:17:36 +0800

    feat: Add styled components.

--- generators/app/templates/business/src/app/components/firstComp.tsx
+++ generators/app/templates/business/src/app/components/firstComp.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import bucket from '../assets/images/bucket.png';
+import styled from 'styled-components';
 
 type CallbackTpes<T> = {
     (say: T) : T
@@ -11,11 +12,18 @@ type Props = {
     Params3: CallbackTpes<boolean>
 }
 
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: green;
+`;
+
 const FirstCom: React.FC<Props> = ({Params1, Params2, Params3}) => {
 
     const show = Params3(true);
 
     return <div>
+        <Title> HELLO styled component.</Title>
         <img src={bucket} />
        {show ? <p> {Params1} is {Params2} now!!! </p> : null } 
     </div>
